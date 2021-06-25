99c0cd87080caf60aaa9be33d7cba125493a2337
gitbash <kolhenagesh2017@gmail.com>
2021-06-25 22:32:20 +0530

    Added: FileIOService

---
+++ EmployeePayrollFileIOService.java
@@ -0,0 +1,13 @@
+package com.jdbcTransactions;
+
+import java.util.List;
+public class EmployeePayrollFileIOService {
+	public void writeData(List<EmployeePayrollData> employeePayrollList, Object ioService) {
+	}
+
+	public void printData() {
+	}
+
+	public void countEntries() {
+	}
+}
